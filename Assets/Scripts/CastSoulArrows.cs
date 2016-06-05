c1b2efa84ea57e55116d5d0b71ddff3f8017faa3
sharkspirit <Antonio.m.pinto@ist.utl.pt>
2016-06-05 21:57:46 +0100

    start commit

---
+++ Assets/Scripts/CastSoulArrows.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CastSoulArrows : MonoBehaviour {
+
+    //Objects
+    public Transform target;
+    public GameObject projectilePrefab;
+
+    //Variable Control
+    public int nProjectiles = 1;
+    public float speed = 1.0f;
+    public float pathAngling = 2.0f;
+    public float radius = 3.0f;
+    public float trackingValue = 0.8f;
+
+    //private variables
+    private List<GameObject> livingProjectiles;
+    private bool castedSpell;
+    private float progressionTime;
+    private float currSpeed;
+    private Vector3 startPos;
+    private Vector3 originalTargetPos;
+    private Vector3 currTargetPos;
+
+    void Start() {
+        livingProjectiles = new List<GameObject>();
+        progressionTime = 0;
+        castedSpell = false;
+        FindTargetNearby(transform.position, radius);
+        startPos = transform.position;
+        castedSpell = true;
+    }
+
+    void Update()
+    {
+       
+        if (target != null)
+        {
+            currTargetPos = target.position;
+            if(castedSpell == true)
+                ManageProjectiles();
+        }
+    }
+
+    public void ManageProjectiles()
+    {
+        if (progressionTime >= 1.0f)
+        {
+            castedSpell = false;
+            KillProjectile();
+            progressionTime = 0.0f;
+            currSpeed = speed;
+            Destroy(this.gameObject);
+        }
+        else
+        if (progressionTime <= 0.0f)
+        {
+            GetProjectileReady();
+            currSpeed = speed;
+            progressionTime += 0.001f * currSpeed;
+        }
+        else
+        {
+            currSpeed = speed * Mathf.Pow((1 + 2f), progressionTime);
+            progressionTime += 0.001f * currSpeed;
+
+            NewProjectilePosition();
+        }
+    }
+
+    private void NewProjectilePosition()
+    {
+        for (int i = 0; i < livingProjectiles.Count; i++)
+        {
+            Vector3 BezierPointVector = RotateVector(this.transform.right * 5, 180 / (livingProjectiles.Count-1) * i);
+
+            Debug.DrawRay(startPos, BezierPointVector, Color.yellow);
+
+            Vector3 newPosition = Mathf.Pow((1 - progressionTime), 2) * startPos + 2 * (1 - progressionTime) * progressionTime *
+            (startPos + BezierPointVector) * pathAngling +
+                Mathf.Pow(progressionTime, 2) * currTargetPos;
+            Vector3 projectileDirection = newPosition - livingProjectiles[i].transform.position;
+
+            Debug.DrawLine(startPos, currTargetPos);
+            Debug.DrawRay(livingProjectiles[i].transform.position, projectileDirection*5 ,Color.red);
+
+            livingProjectiles[i].transform.rotation = Quaternion.LookRotation(projectileDirection);
+            livingProjectiles[i].transform.position = newPosition;
+        }
+    }
+
+    private void GetProjectileReady() {
+        if (livingProjectiles.Count == 0) {
+            for (int i = 0; i < nProjectiles; i++) {
+                livingProjectiles.Add((GameObject)Instantiate(projectilePrefab, this.transform.position, Quaternion.identity));
+            }
+        }
+        else
+        {
+            for (int i = 0; i < livingProjectiles.Count; i++)
+            {
+                livingProjectiles[i].SetActive(true);
+                livingProjectiles[i].transform.position = startPos;
+            }
+        }
+    }
+
+    private void KillProjectile()
+    {
+        for (int i = 0; i < livingProjectiles.Count; i++)
+        {
+            //livingProjectiles[i].SetActive(false);
+            Destroy(livingProjectiles[i]);
+        }
+    }
+
+    private void TrackingCheck()
+    {
+        if(Vector3.Distance(target.position, originalTargetPos) > trackingValue)
+        {
+
+        }
+    }
+
+    private void FindTargetNearby(Vector3 originPos, float radius)
+    {
+        RaycastHit[] sphereHit = Physics.SphereCastAll(originPos, radius, transform.up, 10);
+
+        float closestDist = radius * 2;
+  
+        for (int i = 0; i < sphereHit.Length; i++)
+        {
+            Debug.Log(sphereHit[i].collider.transform.tag);
+            if (sphereHit[i].collider.transform.tag.Equals("Target"))
+            {
+                if(sphereHit[i].distance < closestDist)
+                {
+                    closestDist = sphereHit[i].distance;
+                    target = sphereHit[i].collider.transform;
+                    originalTargetPos = currTargetPos;
+                }
+            }
+        }
+
+    }
+
+    private Vector3 RotateVector(Vector3 vector, float angle)
+    {
+        Vector3 rotatedVector = Quaternion.AngleAxis(angle, this.transform.forward) * vector;
+        return rotatedVector;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(startPos, radius);
+    }
+}
---
+++ Assets/Scripts/ThrowScrolls.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowScrolls : MonoBehaviour {
+
+    public GameObject scroll;
+    public float throwStrength = 5000f;
+    private Rigidbody scrollRB;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            GameObject instScroll = (GameObject)Instantiate(scroll, this.transform.position, Quaternion.identity);
+            scrollRB = instScroll.GetComponent<Rigidbody>();
+            scrollRB.AddForce((transform.forward + transform.up) * throwStrength);
+            scrollRB.AddTorque(transform.right * 10000);
+        }
+    }
+}
