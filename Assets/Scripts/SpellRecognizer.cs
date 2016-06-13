bcc464dec1fa1a0cb394f793ea649c2fc2855105
sharkspirit <Antonio.m.pinto@ist.utl.pt>
2016-06-13 15:29:06 +0100

    start spell maker

--- Assets/Scripts/SpellRecognizer.cs
+++ Assets/Scripts/SpellRecognizer.cs
@@ -12,7 +12,44 @@ public class SpellRecognizer : MonoBehaviour {
 	
 	public string RecognizeSpell(List<Gesture> gestures)
     {
-
+        for (int i = 0; i < gestures.Count; i++) {
+            if (gestures[i].Name.Equals("square"))
+            {
+            }
+            if (gestures[i].Name.Equals("triangle"))
+            {
+            }
+            if (gestures[i].Name.Equals("circle"))
+            {
+            }
+            if (gestures[i].Name.Equals("semicircle"))
+            {
+            }
+            if (gestures[i].Name.Equals("fire"))
+            {
+            }
+            if (gestures[i].Name.Equals("rock"))
+            {
+            }
+            if (gestures[i].Name.Equals("water"))
+            {
+            }
+            if (gestures[i].Name.Equals("wind"))
+            {
+            }
+            if (gestures[i].Name.Equals("lightning"))
+            {
+            }
+            if (gestures[i].Name.Equals("poison"))
+            {
+            }
+            if (gestures[i].Name.Equals("arcane"))
+            {
+            }
+            if (gestures[i].Name.Equals("death"))
+            {
+            }
+        }
         return "";
     } 
 
